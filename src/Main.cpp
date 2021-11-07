b7ccc9edf0e8c41a02524c7ef848a83579230fd1
DorianSzlachcic <329666@uwr.edu.pl>
2021-11-07 23:08:04 +0100

    commit

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
+#include <memory>
 #include <list>
 
 #include "Player.h"
--- src/Player.h
+++ src/Player.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <memory>
 #include <list>
 #include "SoundManager.h"
 #include "Funcs.h"
